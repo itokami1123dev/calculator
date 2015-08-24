ef7ef29109c3e499a6f2a1e781d7ed577e1e7eec
itokami1123dev <kamiari819dev@gmail.com>
2015-08-24 19:53:43 +0900

    20150807

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -2,7 +2,7 @@ package com.example;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String... args) {
         Calculator cal = new Calculator();
 
         System.out.println("-----------------------");
